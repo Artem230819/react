d4dc23c2c82297bca6fbfabb0fad46098149c85b
Artem <faster13rus@gmail.com>
2020-06-24 16:50:39 +0300

    add fille

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+    <header className="header">
+        <img src="https://f990335bdbb4aebc3131-b23f11c2c6da826ceb51b46551bfafdc.ssl.cf2.rackcdn.com/images/5f75d8c7f181c17be50d26ead9b9a88b41e7e567.png" alt=""/>
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://avatars.mds.yandex.net/get-pdb/1527424/0c479c5b-07f5-4d43-b510-5701ced5e534/s1200?webp=false" alt=""/>
+            </div>
+            <div>ava + desc</div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
