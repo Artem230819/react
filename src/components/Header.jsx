37586ff0d0f58bd72f283bc9673ccb11a8b9e6bb
Artem <faster13rus@gmail.com>
2020-06-25 15:22:13 +0300

    css-models (14 lesson)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import c from './Header.module.css'
 
 
 const Header = () => {
     return(
-    <header className="header">
+    <header className={c.header}>
         <img src="https://f990335bdbb4aebc3131-b23f11c2c6da826ceb51b46551bfafdc.ssl.cf2.rackcdn.com/images/5f75d8c7f181c17be50d26ead9b9a88b41e7e567.png" alt=""/>
     </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import c from "./Profile.module.css"
 
 const Profile = () => {
     return(
-        <div className="content">
+        <div className={c.content}>
             <div>
                 <img src="https://avatars.mds.yandex.net/get-pdb/1527424/0c479c5b-07f5-4d43-b510-5701ced5e534/s1200?webp=false" alt=""/>
             </div>
@@ -13,9 +13,9 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+                <div className={c.ports}>
+                    <div className={c.item}>post 1</div>
+                    <div className={c.item}>post 2</div>
                 </div>
             </div>
         </div>
