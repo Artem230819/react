cd8ed5f67a52ef28c853756a63a8ba8044b85059
Artem <faster13rus@gmail.com>
2020-06-27 16:55:35 +0300

    layout (lesson 21)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import c from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Text
+        <div className={c.dialogs}>
+            <div className={c.dialogsItems}>
+                <div className={c.dialog}>
+                    <NavLink to="#" activeClassName={c.active} >Sasha</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="#" activeClassName={c.active}>Artem</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="#" activeClassName={c.active}>Lena</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="#" activeClassName={c.active}>Vika</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="#" activeClassName={c.active}>Gordey</NavLink>
+                </div>
+                <div className={c.dialog}>
+                    <NavLink to="#" activeClassName={c.active}>Ivan</NavLink>
+                </div>
+            </div>
+            <div className="messeges">
+                <div className={c.messege}>Hi</div>
+                <div className={c.messege}>Hi</div>
+                <div className={c.messege}>My name</div>
+                <div className={c.messege}>Good</div>
+            </div>
         </div>
     )
 }
