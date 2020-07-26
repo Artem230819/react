648f10424f4bfc82f7dfc50b2b81c890ed403d58
Artem <faster13rus@gmail.com>
2020-07-27 01:01:28 +0300

    componentDidMount (lesson 54)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,11 @@ import * as axios from "axios";
 import userPhoto from '../../img/user-avatar.png';
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    this.props.setUsers(response.data.items);
-                });
-        }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
     render() {
